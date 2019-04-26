448a518fb9a7155d790e7e9d7d740def1508a9e9
Renato Rosa Franco <Renato_ny@live.com>
2019-04-26 18:16:17 -0300

    Initial commit

---
+++ app/models/member.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+class Member < ApplicationRecord
+  self.table_name = 'members'
+  self.primary_key = 'id'
+
+  validates :name,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 40 }
+  
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 245 }
+end
